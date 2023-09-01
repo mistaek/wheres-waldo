db41bcbb42652720bba19b34fd70a5a833bf17dd
ray <61148858+mistaek@users.noreply.github.com>
2023-09-01 11:27:26 -0400

    Whoopsie wrong file

--- find_waldo.py
+++ find_waldo.py
@@ -1,152 +1,226 @@
-# For running inference on the TF-Hub module.
 import tensorflow as tf
-
-import tensorflow_hub as hub
-
-# For downloading the image.
-import matplotlib.pyplot as plt
-import tempfile
-from six.moves.urllib.request import urlopen
-from six import BytesIO
-
-# For drawing onto the image.
+import tensorflow_datasets as tfds
+from tensorflow import keras
+from tensorflow.keras import optimizers
+import keras_cv
+from pathlib import Path
 import numpy as np
-from PIL import Image
-from PIL import ImageColor
-from PIL import ImageDraw
-from PIL import ImageFont
-from PIL import ImageOps
+from keras_cv import bounding_box
+from keras_cv import utils
+import os
+import resource
+from keras_cv import visualization
+import tqdm
+import pandas as pd
+import matplotlib as mpl
+mpl.use('TkAgg')
 
-# For measuring the inference time.
-import time
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+BATCH_SIZE = 1
+"""
+pretrained_model = keras_cv.models.RetinaNet.from_preset(
+    "retinanet_resnet50_pascalvoc", bounding_box_format="xywh"
+)
 
-module_handle = "https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1"
-detector = hub.load(module_handle).signatures['default']
-
-def display_image(image):
-  fig = plt.figure(figsize=(20, 15))
-  plt.grid(False)
-  plt.imshow(image)
-  plt.show()
-
-def download_and_resize_image(url, new_width=256, new_height=256,
-                              display=False):
-  _, filename = tempfile.mkstemp(suffix=".jpg")
-  response = urlopen(url)
-  image_data = response.read()
-  image_data = BytesIO(image_data)
-  pil_image = Image.open(image_data)
-  pil_image = ImageOps.fit(pil_image, (new_width, new_height), Image.ANTIALIAS)
-  pil_image_rgb = pil_image.convert("RGB")
-  pil_image_rgb.save(filename, format="JPEG", quality=90)
-  print("Image downloaded to %s." % filename)
-  if display:
-    display_image(pil_image)
-  return filename
+filepath = tf.keras.utils.get_file(origin="https://i.imgur.com/gCNcJJI.jpg")
+image = keras.utils.load_img(filepath)
+image = np.array(image)
 
 
-def draw_bounding_box_on_image(image,
-                               ymin,
-                               xmin,
-                               ymax,
-                               xmax,
-                               color,
-                               font,
-                               thickness=4,
-                               display_str_list=()):
-  """Adds a bounding box to an image."""
-  draw = ImageDraw.Draw(image)
-  im_width, im_height = image.size
-  (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
-                                ymin * im_height, ymax * im_height)
-  draw.line([(left, top), (left, bottom), (right, bottom), (right, top),
-             (left, top)],
-            width=thickness,
-            fill=color)
+inference_resizing = keras_cv.layers.Resizing(
+    640, 640, pad_to_aspect_ratio=True, bounding_box_format="xywh"
+)
 
-  # If the total height of the display strings added to the top of the bounding
-  # box exceeds the top of the image, stack the strings below the bounding box
-  # instead of above.
-  display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
-  # Each display_str has a top and bottom margin of 0.05x.
-  total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
+image_batch = inference_resizing([image])
 
-  if top > total_display_str_height:
-    text_bottom = top
-  else:
-    text_bottom = top + total_display_str_height
-  # Reverse list and print from bottom to top.
-  for display_str in display_str_list[::-1]:
-    text_width, text_height = font.getsize(display_str)
-    margin = np.ceil(0.05 * text_height)
-    draw.rectangle([(left, text_bottom - text_height - 2 * margin),
-                    (left + text_width, text_bottom)],
-                   fill=color)
-    draw.text((left + margin, text_bottom - text_height - margin),
-              display_str,
-              fill="black",
-              font=font)
-    text_bottom -= text_height - 2 * margin
+class_ids = [
+    "Aeroplane",
+    "Bicycle",
+    "Bird",
+    "Boat",
+    "Bottle",
+    "Bus",
+    "Car",
+    "Cat",
+    "Chair",
+    "Cow",
+    "Dining Table",
+    "Dog",
+    "Horse",
+    "Motorbike",
+    "Person",
+    "Potted Plant",
+    "Sheep",
+    "Sofa",
+    "Train",
+    "Tvmonitor",
+    "Total",
+]
+class_mapping = dict(zip(range(len(class_ids)), class_ids))
+print(image_batch.shape)
+y_pred = pretrained_model.predict(image_batch)
+print(y_pred)
+"""
 
-def draw_boxes(image, boxes, class_names, scores, max_boxes=10, min_score=0.1):
-  """Overlay labeled boxes on an image with formatted scores and label names."""
-  colors = list(ImageColor.colormap.values())
+inference_resizing = keras_cv.layers.Resizing(
+    1280, 1920, pad_to_aspect_ratio=True, bounding_box_format="xyxy"
+)
 
-  try:
-    font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationSansNarrow-Regular.ttf",
-                              25)
-  except IOError:
-    print("Font not found, using default font.")
-    font = ImageFont.load_default()
-
-  for i in range(min(boxes.shape[0], max_boxes)):
-    if scores[i] >= min_score:
-      ymin, xmin, ymax, xmax = tuple(boxes[i])
-      display_str = "{}: {}%".format(class_names[i].decode("ascii"),
-                                     int(100 * scores[i]))
-      color = colors[hash(class_names[i]) % len(colors)]
-      image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
-      draw_bounding_box_on_image(
-          image_pil,
-          ymin,
-          xmin,
-          ymax,
-          xmax,
-          color,
-          font,
-          display_str_list=[display_str])
-      np.copyto(image, np.array(image_pil))
-  return image
+class_ids = [ 
+    "Waldo"
+]
+class_mapping = dict(zip(range(len(class_ids)), class_ids))
 
 
-print("starting...")
-image_url = "https://upload.wikimedia.org/wikipedia/commons/6/60/Naxos_Taverna.jpg" 
-downloaded_image_path = download_and_resize_image(image_url, 1280, 856, False)
-print("downloaded image", downloaded_image_path)
+def visualize_dataset(inputs, value_range, rows, cols, bounding_box_format):
+    inputs = next(iter(inputs.take(1)))
+    images = inputs["images"]
+    print(utils.to_numpy(images).shape)
+    bounding_boxes = inputs["bounding_boxes"]
+    visualization.plot_bounding_box_gallery(
+        images,
+        value_range=value_range,
+        rows=rows,
+        cols=cols,
+        y_true=bounding_boxes,
+        scale=10,
+        font_scale=0.7,
+        bounding_box_format=bounding_box_format,
+        class_mapping=class_mapping,
+    )
 
 
-def load_img(path):
-  img = tf.io.read_file(path)
-  img = tf.image.decode_jpeg(img, channels=3)
-  return img
+def unpackage_raw(inp, bounding_box_format):
+    dataset = None
+    for ind, row in inp.iterrows():
+        image = keras.utils.load_img(row['filename'])
+        image = np.array(image)
+        sf = min((1280 / image.shape[-3]), (1920/image.shape[-2]))
+        image = inference_resizing(image)
+        data = {
+            "bounding_boxes":{
+                "classes": tf.cast(0, dtype = tf.float32),
+                "boxes": tf.cast([row['xmin'] * sf, row['ymin'] * sf, row['xmax']*sf, row['ymax']*sf], dtype = tf.float32),
+            },
+            "images": tf.cast(image, dtype = tf.float32)
+        }  
+        data["bounding_boxes"]["boxes"] = tf.reshape(data["bounding_boxes"]["boxes"], [1, 1, 1, 4])
+        data["bounding_boxes"]["classes"] = tf.reshape(data["bounding_boxes"]["classes"], [1, 1, 1])
+        data["images"] = tf.reshape(data["images"], [1, 1, 1280, 1920, 3])
+        nds =  tf.data.Dataset.from_tensor_slices(data)
+        if dataset is None:
+            dataset = nds
+        else:
+            dataset = dataset.concatenate(nds)
+    return dataset
 
-def run_detector(detector, path):
-  img = load_img(path)
+def load_data_from_csv(filename, batch_size = BATCH_SIZE, bounding_box_format = "xyxy"):
+    csv = pd.read_csv(filename)
+    csv = unpackage_raw(csv, bounding_box_format)
+    return csv
 
-  converted_img  = tf.image.convert_image_dtype(img, tf.float32)[tf.newaxis, ...]
-  start_time = time.time()
-  result = detector(converted_img)
-  end_time = time.time()
 
-  result = {key:value.numpy() for key,value in result.items()}
 
-  print("Found %d objects." % len(result["detection_scores"]))
-  print("Inference time: ", end_time-start_time)
+train_ds = load_data_from_csv('annotations.csv')
 
-  image_with_boxes = draw_boxes(
-      img.numpy(), result["detection_boxes"],
-      result["detection_class_entities"], result["detection_scores"])
+"""
+train_ds["bounding_boxes"]["boxes"] = tf.reshape(train_ds["bounding_boxes"]["boxes"], [1, 1, 1, 4])
+train_ds["bounding_boxes"]["classes"] = tf.reshape(train_ds["bounding_boxes"]["classes"], [1, 1, 1])
+train_ds["images"] = tf.reshape(train_ds["images"], [1, 1, 1280, 1920, 3])
+print(train_ds["bounding_boxes"]["boxes"].shape) #needs to be (1, 1, 4)
+print(train_ds["bounding_boxes"]["classes"].shape) # needs to be (1, 1)
+print(train_ds["images"].shape) # should be (1, w, h, 3)
 
-  display_image(image_with_boxes)
 
-run_detector(detector, downloaded_image_path)
+tensor = tf.data.Dataset.from_tensor_slices(train_ds)
+"""
+
+visualize_dataset(
+    train_ds, bounding_box_format="xyxy", value_range=(0, 255), rows=1, cols=1
+)
+train_ds = train_ds.prefetch(tf.data.AUTOTUNE)
+def dict_to_tuple(inputs):
+    return inputs["images"], bounding_box.to_dense(
+        inputs["bounding_boxes"], max_boxes=32
+    )
+
+train_ds = train_ds.map(dict_to_tuple, num_parallel_calls=tf.data.AUTOTUNE)
+one = next(iter(train_ds.take(1)))
+print(train_ds.take(1))
+
+class EvaluateCOCOMetricsCallback(keras.callbacks.Callback):
+    def __init__(self, data):
+        super().__init__()
+        self.data = data
+        self.metrics = keras_cv.metrics.BoxCOCOMetrics(
+            bounding_box_format="xyxy",
+            # passing 1e9 ensures we never evaluate until
+            # `metrics.result(force=True)` is
+            # called.
+            evaluate_freq=1e9,
+        )
+
+    def on_epoch_end(self, epoch, logs):
+        self.metrics.reset_state()
+        for batch in tqdm.tqdm(self.data):
+            images, y_true = batch[0], batch[1]
+            y_pred = self.model.predict(images, verbose=0)
+            self.metrics.update_state(y_true, y_pred)
+
+        metrics = self.metrics.result(force=True)
+        logs.update(metrics)
+        return logs
+       
+base_lr = 0.005
+# including a global_clipnorm is extremely important in object detection tasks
+optimizer = tf.keras.optimizers.SGD(
+    learning_rate=base_lr, momentum=0.9, global_clipnorm=10.0
+)
+
+model = keras_cv.models.RetinaNet.from_preset(
+    "resnet50_imagenet",
+    num_classes=len(class_mapping),
+    # For more info on supported bounding box formats, visit
+    # https://keras.io/api/keras_cv/bounding_box/
+    bounding_box_format="xyxy",
+)
+model.compile(
+    classification_loss="focal",
+    box_loss="smoothl1",
+    optimizer=optimizer,
+    # We will use our custom callback to evaluate COCO metrics
+    metrics=None,
+)
+model.fit(
+    train_ds,
+    validation_data = train_ds, # VERY BAD PRACTICE 
+    epochs = 10,
+)
+#model.save('model.keras') # how does loading work?
+
+def visualize_detections(model, dataset, bounding_box_format):
+    images, y_true = next(iter(dataset.take(1)))
+    y_pred = model.predict(images)
+    y_pred = bounding_box.to_ragged(y_pred)
+    visualization.plot_bounding_box_gallery(
+        images,
+        value_range=(0, 255),
+        bounding_box_format=bounding_box_format,
+        y_true=y_true,
+        y_pred=y_pred,
+        scale=4,
+        rows=1,
+        cols=1,
+        show=True,
+        font_scale=0.7,
+        class_mapping=class_mapping,
+    )
+
+
+visualize_detections(model, train_ds, "xyxy")
+
+"""
+visualize_dataset(
+    train_ds, bounding_box_format="xyxy", value_range=(0, 255), rows=1, cols = 1
+)
+#"""
